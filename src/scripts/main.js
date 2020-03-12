a463ef4bb83abe47c20c02dc1a0f7293e062255a
Tata2222 <tataolcha@gmail.com>
2020-03-12 13:05:13 +0200

    header-fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,13 +10,16 @@
   function toggleClass(elem) {
     const burger = document.getElementById('burger');
     const nav = document.getElementById('nav');
+    const phone = document.getElementById('phone');
 
     if (burger.className === 'header__burger active') {
       burger.className = 'header__burger';
       nav.className = 'nav header__nav';
+      phone.className = 'nav__item phone--hidden';
     } else {
       burger.className = 'header__burger active';
       nav.className = 'nav header__nav active';
+      phone.className = 'nav__item phone--hidden active';
     };
   }
 })();
