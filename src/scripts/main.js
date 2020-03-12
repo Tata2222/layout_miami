cf9a9a2c4dcda2db13a6e6376430238232160cac
Tata2222 <tataolcha@gmail.com>
2020-03-12 09:52:52 +0200

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+(function() {
+  onload = function() {
+    document.getElementById('burger').onclick = function() {
+      toggleClass('burger');
+    };
+  };
+
+  function toggleClass(elem) {
+    const burger = document.getElementById('burger');
+    const nav = document.getElementById('nav');
+
+    if (burger.className === 'header__burger active') {
+      burger.className = 'header__burger';
+      nav.className = 'nav header__nav';
+    } else {
+      burger.className = 'header__burger active';
+      nav.className = 'nav header__nav active';
+    };
+  }
+})();
